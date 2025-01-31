10a1329623d58b4a4da72bca696e6ae0ad303f28
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2025-01-31 12:54:49 +0100

    gitignore updated (.idea and target removed) git status q
    qq

--- src/main/java/tdd/Calculator.java
+++ src/main/java/tdd/Calculator.java
@@ -3,8 +3,11 @@ package tdd;
 public class Calculator {
 
     public float add(float a, float b){
-        System.out.println("Give me a sec..");
         return a + b;
+    }
+
+    public float add(String numbers){
+        count
     }
 
     public float subtract(float a, float b){
